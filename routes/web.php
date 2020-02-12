6fcc1fb412c302c92f4ee6b88336fe9c02608b3c
Matheus Adler <Adler@imac-robert.mirante.com.br>
2020-02-12 13:04:32 -0300

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,25 @@
 <?php
 
+Route::get('/login', function(){
+    return 'Login';
+})->name('login');
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/categoria/{flag}', function($flag){
+    return "Produto da categoria: {$flag}";
+});
+
+Route::get('admin/dashboard', function(){
+    return 'Home Admin';
+})->middleware('auth');
+
+Route::get('admin/financeiro', function(){
+    return 'Financeiro Admin';
+})->middleware('auth');
+
+Route::get('admin/rh', function(){
+    return 'RH Admin';
+})->middleware('auth');
